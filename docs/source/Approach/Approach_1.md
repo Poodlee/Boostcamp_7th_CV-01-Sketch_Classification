42e7960f47569abd2ba3faebaeda49efef5fe5a2
Poodlee <Poodlee@gmail.com>
2024-09-10 08:04:47 +0900

    First Layout Write

---
+++ docs/source/Approach/Approach_1.md
@@ -0,0 +1,17 @@
+# Approach_1
+
+## Hello
+
+* Approach_1-1
+* Approach_1-1
+
+### Test Code
+
+```python
+for i in range(0,100):
+    print(i)
+print('hello')
+```
+
+Hello, Today
+Today's ```weather``` is so hotttt.
---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Sketch Image Classification'
+copyright = '2024, Park Dong Jun'
+author = 'Park Dong Jun'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ko'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_favicon = 'favicon.ico'
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+
+##############################################################################
